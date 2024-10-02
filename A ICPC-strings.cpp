8f3ab6c622e2e2a7f5aad80df997f8c454c91983
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-10-02 16:16:08 +0600

    Create A ICPC-strings.cpp

---
+++ A ICPC-strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    
+    cout << 12 << endl;
+    
+    return 0;
+}
+/*
+n≥4, the number of valid ICPC-strings is always 12 because the structure repeats every 4 characters, and there are 12 valid permutations of "ICPC".*/
